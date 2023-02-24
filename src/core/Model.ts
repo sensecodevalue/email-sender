2c9a38b34e8a89848426740e8acd839cbbb868dc
정대윤 <sensecodevalue@gmail.com>
2023-02-25 03:23:22 +0900

    refactor: remove model test

--- src/core/Model.ts
+++ src/core/Model.ts
@@ -8,7 +8,6 @@ export interface FindParams {
 export type FindByIdParam = number | string;
 
 interface InterfaceModal<M> {
-  dao: unknown;
   create?: (form: M) => M;
   findById?: (id: FindByIdParam) => M;
   find?: (params: FindParams) => M[];
@@ -17,9 +16,9 @@ interface InterfaceModal<M> {
 }
 
 export abstract class Model<M> implements InterfaceModal<M> {
-  dao: unknown;
+  #dao: unknown;
   constructor(dao: unknown) {
-    this.dao = dao;
+    this.#dao = dao;
   }
 
   abstract create(form: M): M;
@@ -27,35 +26,4 @@ export abstract class Model<M> implements InterfaceModal<M> {
   abstract find(params: FindParams): M[];
   abstract update(id: FindByIdParam, form: Partial<Omit<M, "id">>): M;
   abstract delete(id: FindByIdParam): M;
-}
-
-interface IM1 {}
-
-class M1 extends Model<IM1> {
-  constructor(dao: unknown) {
-    super(dao);
-  }
-
-  create(form: IM1) {
-    // this.dao.create(form);
-    return { name: "1", age: 1 };
-  }
-  find(params: FindParams) {
-    // this.dao.find(params);
-    return [{ name: "1", age: 1 }];
-  }
-  findById(id: FindByIdParam) {
-    // this.dao.findById(id);
-    return { name: "1", age: 1 };
-  }
-
-  update(id: FindByIdParam, form: Partial<Omit<IM1, "id">>) {
-    // this.dao.update(id, form);
-    return { name: "1", age: 1 };
-  }
-
-  delete(id: FindByIdParam) {
-    // this.dao.delete(id);
-    return { name: "1", age: 1 };
-  }
 }
