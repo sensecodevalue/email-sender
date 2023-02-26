efd81960be2002529c30e1ed369da37194cd77aa
정대윤 <sensecodevalue@gmail.com>
2023-02-27 01:37:35 +0900

    feat: use core/Server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import "./lib/common/env";
 
-import { dbClient } from "./database/dao";
+import bodyParser from 'body-parser';
+
+import Server from './core/Server';
+import TemplateModule from "./apps/template/module/template.module";
+import ReceiversModule from './apps/receivers/module/receivers.module';
+
+const server = new Server();
+
+server.app.use(bodyParser.urlencoded({ extended: false }));
+
+server.useController(ReceiversModule.receiversController);
+server.useController(TemplateModule.senderController);
+
+server.listen();
